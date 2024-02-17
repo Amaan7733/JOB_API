9d1399c2c9bda0ec134308a60af8347a4538d542
Amaan7733 <syedamaan7733@gmail.com>
2024-02-17 12:26:17 +0530

    seaggerUI updatation

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocx = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -36,8 +41,10 @@ const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
 
 app.get("/", (req, res) => {
-  res.send("Jobs API");
+  res.send('<h1> JOBS API </h1> <a href="/api-docs>Documantation </a>"');
 });
+app.use("api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocx));
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
